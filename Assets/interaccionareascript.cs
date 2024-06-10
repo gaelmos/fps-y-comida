b97670be2fe9229784b0d1d9a1c0e86eb0bbc5cf
gaelmos <156100424+gaelmos@users.noreply.github.com>
2024-06-10 16:01:31 -0300

    tarea casi terminada

---
+++ Assets/interaccionareascript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class interaccionareascript : MonoBehaviour
+{
+    public Text puntostxt;   
+    public int puntosactuales = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void puntos(int puntosalimentos)
+    {
+        puntosactuales += puntosalimentos;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        pickupscript pikup;
+        pikup = GetComponent<pickupscript>();
+        if(pikup.escomida == true)
+        {
+            puntos(pikup.puntosalimento);
+            Destroy(other.gameObject);
+            
+        }
+        else
+        {
+            puntos(pikup.puntosalimento);
+        }
+        Debug.Log(other);
+
+    }
+}
